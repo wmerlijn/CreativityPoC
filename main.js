cf94e49d0e8bd0dae5a23aba833fb7ec6243bf15
Merlijn <409326@student.fontys.nl>
2022-06-16 17:00:10 +0200

    Met Docent Maikel de code afgemaakt. Animatie loopt nu soepel en knop kan pas ingedrukt worden wanneer deze gereset is

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ function knopClick() {
     clickCounter++;
     console.log(clickCounter);
 
+    document.getElementById("clicks").innerHTML = clickCounter;
+    if(!button.disabled){ disableButton(); }
 };
 //Selecteren van de button
 const button = document.querySelector('#button');
@@ -15,18 +17,24 @@ const button = document.querySelector('#button');
 //functie die button uitschakeld
 
 function disableButton() {
+    clearTimeout(button.classremover)
   console.log("Disable");
     button.disabled = true;
     //Button word steeds langer niet clickbaar
     setTimeout(enableButton, clickCounter * 100);
+    button.classList.add("disabled");
+    button.classremover=setTimeout(()=>{
+        button.classList.remove("disabled");
+    },clickCounter*50)
 };
 
 //functie de de button weer aanzet
 function enableButton() {
     console.log("Enable");
       button.disabled = false;
-
+      
+    button.style.setProperty("--transitionTime", (clickCounter * 50) + "ms")
   };
 
 //Uitschakelen button bij click
-button.addEventListener('click', disableButton);
+button.addEventListener('click', knopClick);
