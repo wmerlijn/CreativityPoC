0d039dcbf560e1b307d7f299b4ee9d179ad2a31a
Merlijn <409326@student.fontys.nl>
2022-06-16 13:54:43 +0200

    Comments toegevoegd

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
 let clickCounter = 0;
 
 
-
+//bijhouden hoevaak de knop is ingedrukt
 function knopClick() {
     console.log("test");
     clickCounter++;
     console.log(clickCounter);
 
 };
-
-
-
-
-
-
-
+//Selecteren van de button
 const button = document.querySelector('#button');
+
+
+//functie die button uitschakeld
 
 function disableButton() {
   console.log("Disable");
     button.disabled = true;
-    setTimeout(enableButton, clickCounter * 1000);
+    //Button word steeds langer niet clickbaar
+    setTimeout(enableButton, clickCounter * 100);
 };
 
+//functie de de button weer aanzet
 function enableButton() {
     console.log("Enable");
       button.disabled = false;
 
   };
 
-
-
+//Uitschakelen button bij click
 button.addEventListener('click', disableButton);
