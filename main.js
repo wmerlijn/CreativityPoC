c1a5315724be20b36db6ddcbc43146bcc289f25e
Merlijn <409326@student.fontys.nl>
2022-06-16 13:49:38 +0200

    Button aangemaakt plus teller functie

--- main.js
+++ main.js
@@ -8,3 +8,27 @@ function knopClick() {
     console.log(clickCounter);
 
 };
+
+
+
+
+
+
+
+const button = document.querySelector('#button');
+
+function disableButton() {
+  console.log("Disable");
+    button.disabled = true;
+    setTimeout(enableButton, clickCounter * 1000);
+};
+
+function enableButton() {
+    console.log("Enable");
+      button.disabled = false;
+
+  };
+
+
+
+button.addEventListener('click', disableButton);
